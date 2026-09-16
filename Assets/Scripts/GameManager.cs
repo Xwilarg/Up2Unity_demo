2799538593b674fc5a47ff7eabd0c3d25f99770d
Xwilarg <xwilarg@protonmail.com>
2026-09-16 22:59:41 +0200

    Update use of endpoint

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,36 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    private TMP_Text _status;
+
+    [SerializeField]
     private string clientId, gameSlug;
 
-    private static Up2JamManager _up2jam;
+    private static Up2UnityManager _up2jam;
 
-    private void Awake()
+    private async void Awake()
     {
+        _status.text = "Waiting for connection...";
         if (_up2jam == null)
         {
             _up2jam = new(clientId, gameSlug);
-            StartCoroutine(_up2jam.ConnectCoroutine((info) =>
+            var res = await _up2jam.Connect();
+
+            _status.text = $"Waiting for validation, user code: {res.UserCode}";
+            Application.OpenURL(res.VerificationUri);
+
+            while (true)
             {
-            }));
+                await Awaitable.WaitForSecondsAsync(.5f);
+                if (await _up2jam.ValidateToken())
+                {
+                    _status.text = "Connected!";
+                    return;
+                }
+            }
         }
     }
 }
